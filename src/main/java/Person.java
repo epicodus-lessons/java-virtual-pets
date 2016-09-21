f0ea7255778424b7c8fcb1837a3cbbf4a77eedfc
Courtney Phillips <courtney@epicodus.com>
2016-09-21 12:47:20 -0700

    Declare email attribute in Person class, include in constructor and write corresponding getter method.

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,12 +1,18 @@
 public class Person {
   private String name;
+  private String email;
 
-  public Person(String name) {
+  public Person(String name, String email) {
     this.name = name;
+    this.email = email;
   }
 
   public String getName() {
     return name;
+  }
+
+  public String getEmail() {
+    return email;
   }
 
 }
