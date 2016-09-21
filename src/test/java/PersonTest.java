01a4bc356a413a1452d8f9f5ab83b0ca98eb7ff1
Courtney Phillips <courtney@epicodus.com>
2016-09-21 13:40:09 -0700

    Write test asserting all() method successfully retrieves multiple Person instances from database.

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -38,4 +38,14 @@ public class PersonTest {
     assertTrue(Person.all().get(0).equals(testPerson));
   }
 
+  @Test
+  public void all_returnsAllInstancesOfPerson_true() {
+    Person firstPerson = new Person("Henry", "henry@henry.com");
+    firstPerson.save();
+    Person secondPerson = new Person("Harriet", "harriet@harriet.com");
+    secondPerson.save();
+    assertEquals(true, Person.all().get(0).equals(firstPerson));
+    assertEquals(true, Person.all().get(1).equals(secondPerson));
+  }
+
 }
