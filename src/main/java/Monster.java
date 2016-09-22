fc9f2f77d930743e8f83698899980770757b060f
Courtney Phillips <courtney@epicodus.com>
2016-09-22 12:14:37 -0700

    Override equals() method to correctly compare Monster objects from database.

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -15,4 +15,15 @@ public class Monster {
     return personId;
   }
 
+  @Override
+  public boolean equals(Object otherMonster){
+    if (!(otherMonster instanceof Monster)) {
+      return false;
+    } else {
+      Monster newMonster = (Monster) otherMonster;
+      return this.getName().equals(newMonster.getName()) &&
+             this.getPersonId() == newMonster.getPersonId();
+    }
+  } 
+
 }
