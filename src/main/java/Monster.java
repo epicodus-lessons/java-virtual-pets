7dca23c4219cbc217665edbbf1db28b133867da2
Courtney Phillips <courtney@epicodus.com>
2016-09-26 14:42:25 -0700

    Refactor Monster class to be abstract.

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -5,33 +5,33 @@ import java.sql.Timestamp;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class Monster {
+public abstract class Monster {
 
-  private String name;
-  private int personId;
-  private int id;
-  private int foodLevel;
-  private int sleepLevel;
-  private int playLevel;
-  private Timestamp birthday;
-  private Timestamp lastSlept;
-  private Timestamp lastAte;
-  private Timestamp lastPlayed;
-  private Timer timer;
+  public String name;
+  public int personId;
+  public int id;
+  public int foodLevel;
+  public int sleepLevel;
+  public int playLevel;
+  public Timestamp birthday;
+  public Timestamp lastSlept;
+  public Timestamp lastAte;
+  public Timestamp lastPlayed;
+  public Timer timer;
 
   public static final int MAX_FOOD_LEVEL = 3;
   public static final int MAX_SLEEP_LEVEL = 8;
   public static final int MAX_PLAY_LEVEL = 12;
   public static final int MIN_ALL_LEVELS = 0;
 
-  public Monster(String name, int personId) {
-    this.name = name;
-    this.personId = personId;
-    playLevel = MAX_PLAY_LEVEL / 2;
-    sleepLevel = MAX_SLEEP_LEVEL / 2;
-    foodLevel = MAX_FOOD_LEVEL / 2;
-    timer = new Timer();
-  }
+  // public Monster(String name, int personId) {
+  //   this.name = name;
+  //   this.personId = personId;
+  //   playLevel = MAX_PLAY_LEVEL / 2;
+  //   sleepLevel = MAX_SLEEP_LEVEL / 2;
+  //   foodLevel = MAX_FOOD_LEVEL / 2;
+  //   timer = new Timer();
+  // }
 
   public String getName(){
     return name;
