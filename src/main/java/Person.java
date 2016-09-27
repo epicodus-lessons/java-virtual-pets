c70ae5b1e70ffa7d2e628ae341dbb687f2f4a430
Courtney Phillips <courtney@epicodus.com>
2016-09-27 12:36:10 -0700

    Add getCOmmunities() method and corresponding test to return all communities a person belongs to.

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -75,4 +75,24 @@ public class Person {
       return allMonsters;
   }
 
+  public List<Community> getCommunities() {
+    try(Connection con = DB.sql2o.open()){
+      String joinQuery = "SELECT community_id FROM communities_persons WHERE person_id = :person_id";
+      List<Integer> communityIds = con.createQuery(joinQuery)
+        .addParameter("person_id", this.getId())
+        .executeAndFetch(Integer.class);
+
+      List<Community> communities = new ArrayList<Community>();
+
+      for (Integer communityId : communityIds) {
+        String communityQuery = "SELECT * FROM communities WHERE id = :communityId";
+        Community community = con.createQuery(communityQuery)
+          .addParameter("communityId", communityId)
+          .executeAndFetchFirst(Community.class);
+        communities.add(community);
+      }
+      return communities;
+    }
+  }
+
 }
