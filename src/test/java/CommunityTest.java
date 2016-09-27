12405d364f8b3916b845395037436118f5e1575e
Courtney Phillips <courtney@epicodus.com>
2016-09-27 10:56:58 -0700

    Create CommunityTest class, write first test asserting Community objects can be instantiated."

---
+++ src/test/java/CommunityTest.java
@@ -0,0 +1,17 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.sql2o.*;
+import java.util.Arrays;
+
+public class CommunityTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void community_instantiatesCorrectly_true() {
+    Community testCommunity = new Community("Water Enthusiasts", "Lovers of all things water monsters!");
+    assertEquals(true, testCommunity instanceof Community);
+  }
+
+}
