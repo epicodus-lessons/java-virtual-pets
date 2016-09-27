3ceda5f7ada177669460cf837e1ecdd386f1462c
Courtney Phillips <courtney@epicodus.com>
2016-09-26 18:56:54 -0700

    Update save() method in Monster to save type information.

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -134,10 +134,11 @@ public abstract class Monster {
 
   public void save() {
     try(Connection con = DB.sql2o.open()) {
-      String sql = "INSERT INTO monsters (name, personId, birthday) VALUES (:name, :personId, now())";
+      String sql = "INSERT INTO monsters (name, personId, birthday, type) VALUES (:name, :personId, now(), :type)";
       this.id = (int) con.createQuery(sql, true)
         .addParameter("name", this.name)
         .addParameter("personId", this.personId)
+        .addParameter("type", this.type)
         .executeUpdate()
         .getKey();
     }
